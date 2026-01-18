276a9ea595a6be1e34bb3b9a1ab83662d547f397
kabid <andamasovazat@gmail.com>
2026-01-18 21:27:25 +0500

    IT1: add core application structure by Azat

--- src/com/company/controllers/interfaces/IAccountController.java
+++ src/com/company/controllers/interfaces/IAccountController.java
@@ -1,4 +1,7 @@
 ﻿package com.company.controllers.interfaces;
 
 public interface IAccountController {
+    void createAccount(int userId, String name, double initialBalance);
+    void showAccounts(int userId);
+    void showAccountDetails(int accountId);
 }
